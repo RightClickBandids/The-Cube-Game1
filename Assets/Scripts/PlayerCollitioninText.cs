7e8aaefc15859748cc8194f9807211fa195c1bd5
TheErraticCookie <aram.marquez01@gmail.com>
2020-07-16 12:46:11 -0500

    ScoreUpdate2

---
+++ Assets/Scripts/PlayerCollitioninText.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollitioninText : MonoBehaviour
+{
+    
+    public Text score;
+
+void OnCollisionEnter(Collision CollisionInfo)
+{
+
+    //What happens if collision with player
+
+    if (CollisionInfo.collider.tag == "Obstacles")
+    {
+        
+        
+        score.enabled = false;
+
+    }
+    if (CollisionInfo.collider.tag == "Finish")
+    {
+        
+       
+        score.enabled = false;
+
+    }
+}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform player;
+    public Text Scoretext;
+    public float Scoremultiplier;
+
+    void Start()
+    {
+        //Multiplier of score
+        Scoremultiplier = 3.696f;
+    }
+
+   //Score based on position
+    void Update()
+    {
+
+        //Debug.Log(player.position.z - 28.4);
+
+        Scoretext.text = (Scoremultiplier * (-player.position.z + 28.4)).ToString("0");
+      
+     }
+}
